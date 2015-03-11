1295ca605ced52fba1b00e09d0cee487a0ddde32
Huang Bo <huangbop@gmail.com>
2015-03-11 19:02:05 +0800

    Ken burns view

---
+++ app/src/main/java/com/huangbop/takeout/BaseActivity.java
@@ -0,0 +1,16 @@
+package com.huangbop.takeout;
+
+import android.app.Activity;
+import android.widget.Toast;
+
+/**
+ * Created by Administrator on 2015/3/11.
+ */
+public class BaseActivity extends Activity {
+
+  public void toast(String msg) {
+    Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+  }
+
+}
+
--- app/src/main/java/com/huangbop/takeout/SplashActivity.java
+++ app/src/main/java/com/huangbop/takeout/SplashActivity.java
@@ -1,41 +1,50 @@
 package com.huangbop.takeout;
 
-import android.app.Activity;
-import android.graphics.Matrix;
-import android.graphics.drawable.AnimationDrawable;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.animation.Animation;
-import android.view.animation.TranslateAnimation;
-import android.widget.ImageView;
 
-import java.util.logging.Handler;
+import com.flaviofaria.kenburnsview.KenBurnsView;
+import com.flaviofaria.kenburnsview.Transition;
 
 import cn.bmob.v3.Bmob;
 
 /**
  * Created by Administrator on 2015/3/10.
  */
-public class SplashActivity extends Activity {
+public class SplashActivity extends BaseActivity {
 
   private static final String APPID = "e9c72808f8555c8d7846a13a50e907a6";
 
+  private KenBurnsView bgView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_splash);
 
+//    Bmob
     Bmob.initialize(this, APPID);
 
-    ImageView bgview = (ImageView) findViewById(R.id.bgView);
-    bgview.setBackgroundResource(R.drawable.bg);
+//  bg
+    bgView = (KenBurnsView) findViewById(R.id.bgView);
+    bgView.setTransitionListener(new KenBurnsView.TransitionListener() {
+      @Override
+      public void onTransitionStart(Transition transition) {
+        toast("trans start");
+      }
 
-    TranslateAnimation trans = new TranslateAnimation(0, 120, 0, 120);
-    trans.setDuration(5000);
-    bgview.setAnimation(trans);
+      @Override
+      public void onTransitionEnd(Transition transition) {
+        toast("trans end");
 
+        Intent intent;
+        intent = new Intent(SplashActivity.this, MainActivity.class);
+        startActivity(intent);
 
+      }
+    });
 
+//
 
 
   }
