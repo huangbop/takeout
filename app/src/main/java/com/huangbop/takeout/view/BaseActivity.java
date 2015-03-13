acd3985cede15141ce28e490d87cb149033e67e4
Huang Bo <huangbop@gmail.com>
2015-03-14 06:11:07 +0800

    Splash

--- app/src/main/java/com/huangbop/takeout/view/BaseActivity.java
+++ app/src/main/java/com/huangbop/takeout/view/BaseActivity.java
@@ -1,12 +1,26 @@
 package com.huangbop.takeout.view;
 
 import android.app.Activity;
+import android.os.Bundle;
 import android.widget.Toast;
+
+import cn.bmob.v3.Bmob;
 
 /**
  * Created by Administrator on 2015/3/11.
  */
 public class BaseActivity extends Activity {
+
+  private static final String APPID = "e9c72808f8555c8d7846a13a50e907a6";
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    //    Bmob
+    Bmob.initialize(this, APPID);
+
+  }
 
   public void toast(String msg) {
     Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
--- app/src/main/java/com/huangbop/takeout/view/SplashActivity.java
+++ app/src/main/java/com/huangbop/takeout/view/SplashActivity.java
@@ -2,25 +2,16 @@ package com.huangbop.takeout.view;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
-import android.view.animation.DecelerateInterpolator;
-import android.view.animation.LinearInterpolator;
 
 import com.flaviofaria.kenburnsview.KenBurnsView;
-import com.flaviofaria.kenburnsview.RandomTransitionGenerator;
-import com.flaviofaria.kenburnsview.Transition;
 import com.huangbop.takeout.R;
-
-import cn.bmob.v3.Bmob;
 
 /**
  * Created by Administrator on 2015/3/10.
  */
 public class SplashActivity extends BaseActivity {
-
-  private static final String APPID = "e9c72808f8555c8d7846a13a50e907a6";
 
   private KenBurnsView bgView;
 
@@ -29,13 +20,8 @@ public class SplashActivity extends BaseActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_splash);
 
-//    Bmob
-    Bmob.initialize(this, APPID);
-
-//  bg
+//  splash bg
     bgView = (KenBurnsView) findViewById(R.id.bgView);
-    bgView.setImageResource(R.drawable.bg);
-    bgView.setTransitionGenerator(new RandomTransitionGenerator(20000, new DecelerateInterpolator()));
     AlphaAnimation alpha = new AlphaAnimation(1.0f, 1.0f);
     alpha.setDuration(3000);
     bgView.setAnimation(alpha);
@@ -59,14 +45,6 @@ public class SplashActivity extends BaseActivity {
       }
     });
 
-
-
-//
-
-
   }
-
-
-
 
 }
